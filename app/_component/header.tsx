63ad744f22c0bc404b64e86c5eb83a4d5734e54d
unknown <captainzds007@gmail.comgit config --global user.name>
2024-03-16 03:33:28 +0500

    initialize

---
+++ app/_component/header.tsx
@@ -0,0 +1,11 @@
+import Link from "next/link"
+
+export const Header = () => {
+    return (
+        <nav>
+            <Link href="" className="text-[#eee]">Home</Link>
+            <Link href="" className="text-[#eee]">Contacts</Link>
+            <Link href="" className="text-[#eee]">Info</Link>
+        </nav>
+    )
+}
